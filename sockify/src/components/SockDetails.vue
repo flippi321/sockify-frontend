7f02c03da2c4b09ed1584b5b811a3da55926383f
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 01:50:16 +0200

    Added a super basic detail page

---
+++ sockify/src/components/SockDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <h2> Type: {{ type }} </h2>
+    <h2> {{ description }} </h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['name', 'description', 'type'],
+}
+</script>
