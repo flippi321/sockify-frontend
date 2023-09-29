7bd4a5f82933b4e9c7ce24beb8354f69ababbb94
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 00:42:52 +0200

    Added an imput box with placeholder text

--- sockify/src/components/MainPage.vue
+++ sockify/src/components/MainPage.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="hello">
-    <h1>{{ "Your Prompt:" }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
+  <div>
+    <p>{{ msg }}</p>
+    <input type="text" :placeholder="randomPlaceholder">
+    <p>{{ response }}</p>
   </div>
 </template>
 
