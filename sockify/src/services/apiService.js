4c565fc5b3f4a9c193cb1b22b211628fed9e94b8
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 01:48:51 +0200

    Changed URL

--- sockify/src/services/apiService.js
+++ sockify/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000/api";  // e.g., "http://localhost:3000/api"
+const API_URL = "http://localhost:3000/";
 
 export default {
     async fetchData() {
