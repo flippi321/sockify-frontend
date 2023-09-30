08755910ca1871b2d7ef857baa8f3886edb30ae1
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 04:28:00 +0200

    Added quality to API call

--- sockify/src/services/apiService.js
+++ sockify/src/services/apiService.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 const API_URL = "http://192.168.10.149:3000/sockIdea"; // or "http://localhost:3000/"
 
 export default {
-    async getASockIdea(size, type) {
+    async getASockIdea(size, type, quality) {
         try {
             const sockDescription = {
                 size: size,
-                type: type
+                type: type,
+                quality: quality,
             };
             const response = await axios.post(`${API_URL}`, sockDescription);
             return response.data;
