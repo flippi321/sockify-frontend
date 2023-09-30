822534aff49b0d24cf8047c97045b14bedd86295
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 04:07:55 +0200

    Overhauled detail page UI

--- sockify/src/components/SockDetails.vue
+++ sockify/src/components/SockDetails.vue
@@ -1,14 +1,52 @@
 <template>
-  <div>
-    <p>{{ name }}</p>
-    <h2> Type: {{ type }} </h2>
-    <h2> {{ description }} </h2>
+<p> You got: </p>
+  <div class="container">
+    <h1>{{ name }}</h1>
+    <h3> {{ type }} </h3>
+    <p> {{ description }} </p>
   </div>
 </template>
 
 <script>
-
 export default {
   props: ['name', 'description', 'type'],
 }
 </script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    margin: 20px;
+    background-color: #f9f9f9;
+    max-width: 400px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+h1 {
+    color: #333;
+    font-size: 2.5rem;
+    margin-bottom: 15px;
+    font-weight: 600;
+}
+
+h3 {
+    color: #555;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+    font-weight: 500;
+}
+
+p {
+    color: #666;
+    font-size: 1rem;
+    font-weight: 400;
+    text-align: center;
+}
+</style>
