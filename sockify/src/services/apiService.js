6009546afd9b642049152afb482e3eb50c0f5add
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 03:34:22 +0200

    Updated axios call

--- sockify/src/services/apiService.js
+++ sockify/src/services/apiService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = "http://192.168.10.149:3000/"; // or "http://localhost:3000/"
+const API_URL = "http://192.168.10.149:3000/sockIdea"; // or "http://localhost:3000/"
 
 export default {
     async getASockIdea() {
         try {
-            const response = await axios.get(`${API_URL}`);
+            const response = await axios.post(`${API_URL}`);
             return response.data;
         } catch (error) {
             console.error("Error fetching data from backend:", error);
