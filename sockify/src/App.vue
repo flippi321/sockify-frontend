bbef60750641f7825285a1c4b3bdb3c67cd06f9c
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 02:32:46 +0200

    Made it so that the user is sent to the detail page when a sock is generated

--- sockify/src/App.vue
+++ sockify/src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <MainPage msg="Socks made by you:"/>
+  <div>
+    <MainPage v-if="!sockData" msg="Socks made by you:" @generatedSock="showSock"/>
+    <SockDetails v-else :name="sockData.name" :description="sockData.description" :type="sockData.type" />
+  </div>
 </template>
 
 <script>
 import MainPage from './components/MainPage.vue'
-
+import SockDetails from './components/SockDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    MainPage
+    MainPage,
+    SockDetails
+  },
+  data() {
+    return {
+      sockData: null
+    }
+  },
+  methods: {
+    showSock(sockCSV) {
+      const [name, type, description] = sockCSV.split(',').map(str => str.trim());
+
+      this.sockData = {
+        name,
+        type,
+        description
+      };
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
