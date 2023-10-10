1c19001e5c09754da8ff82205188012a884f531d
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-10 16:28:57 +0200

    Updated SockDetails UI

--- src/components/SockDetails.vue
+++ src/components/SockDetails.vue
@@ -1,22 +1,22 @@
 <template>
-  <div>
-    <p> You got: </p>
-    <div class="container">
-      <h1>{{ name }}</h1>
-      <!-- Display the image -->
-      <img :src="imageUrl" alt="Sock Image" v-if="imageUrl">
-      <h3> {{ type }} </h3>
-      <h4> {{ slogan }} </h4>
-      <p> {{ description }} </p>    
+  <div class="container">
+      <div class="image-container">
+        <img :src="imageUrl" alt="Sock Image" v-if="imageUrl">
+      </div>
+      <div class="info">
+        <div class="box name"><h1>{{ name }}</h1></div>
+        <div class="box type"><h3>{{ type }}</h3></div>
+        <div class="box slogan"><h4>{{ slogan }}</h4></div>
+        <div class="box description"><p>{{ description }}</p></div>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
 import apiService from '../services/apiService.js';
 
 export default {
-  props: ['name', 'type', 'slogan', 'description'],
+  props: ['name', 'type', 'theme', 'slogan', 'description'],
   data() {
     return {
       imageUrl: ''
@@ -24,6 +24,7 @@ export default {
   },
   async mounted() {
     try {
+      // Fetch image
       const imageData = await apiService.getASockImage(this.name, this.type, this.description);
       this.imageUrl = imageData;
       console.log(imageData)
@@ -37,43 +38,68 @@ export default {
 <style scoped>
 .container {
     display: flex;
-    flex-direction: column;
+    background-color: white;
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+.image-container {
+    flex: 1;
+    background-color: #f6f6f6;
+    display: flex;
     align-items: center;
+    justify-content: center;
     padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    margin: 20px;
-    background-color: #f9f9f9;
-    max-width: 400px;
-    margin-left: auto;
-    margin-right: auto;
+}
+
+.info {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    padding: 20px;
+}
+
+.box {
+    padding: 10px 15px;
+    background-color: #f6f6f6;
+    border: 1px solid #e1e1e1;
+    border-radius: 5px;
+}
+
+h1, h3, h4, p {
+    margin: 0;  
 }
 
 h1 {
-    color: #333;
-    font-size: 2.5rem;
-    margin-bottom: 15px;
+    color: #2c2c2c;
+    font-size: 2rem;
     font-weight: 600;
 }
 
 h3 {
-    color: #555;
+    color: #4a4a4a;
     font-size: 1.5rem;
-    margin-bottom: 10px;
     font-weight: 500;
 }
 
+h4 {
+    color: #616161;
+    font-size: 1.3rem;
+}
+
 p {
-    color: #666;
+    color: #737373;
     font-size: 1rem;
     font-weight: 400;
-    text-align: center;
 }
 
 img {
     max-width: 100%;
-    margin-top: 15px;
-    border-radius: 8px;
+    border-radius: 10px;
 }
+
 </style>
